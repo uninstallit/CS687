575bbddfffbaa27d0461c31322289748b133afaf
Edvin Beqari <edbeqari@vt.edu>
2022-11-03 19:47:48 -0400

    simulate keyboard

--- pong.py
+++ pong.py
@@ -1,7 +1,19 @@
 # Import required library
 import turtle
+from pynput.keyboard import Key, Controller
+import random
+import time
 
 # source: https://www.geeksforgeeks.org/create-pong-game-using-python-turtle/
+
+# run faster
+# https://stackoverflow.com/questions/59119036/how-do-i-make-a-turtle-run-faster
+
+# simulate key presses
+# https://www.youtube.com/watch?v=nQTWe5PYDwM&t=107s&ab_channel=Pydemy
+
+
+keyboard = Controller()
 
 # Create screen
 sc = turtle.Screen()
@@ -9,12 +21,39 @@ sc.title("Pong game")
 sc.bgcolor("white")
 sc.setup(width=1000, height=600)
 
+# Midfield
+midfield = turtle.Turtle()
+midfield.speed(1)
+midfield.shape("square")
+midfield.color("black")
+midfield.shapesize(stretch_wid=25, stretch_len=0.2)
+midfield.penup()
+midfield.goto(0, 0)
+
+# Left Penalty
+midfield = turtle.Turtle()
+midfield.speed(1)
+midfield.shape("square")
+midfield.color("black")
+midfield.shapesize(stretch_wid=25, stretch_len=0.2)
+midfield.penup()
+midfield.goto(-250, 0)
+
+# Right Penalty
+midfield = turtle.Turtle()
+midfield.speed(1)
+midfield.shape("square")
+midfield.color("black")
+midfield.shapesize(stretch_wid=25, stretch_len=0.2)
+midfield.penup()
+midfield.goto(250, 0)
+
 
 # Left paddle
 left_pad = turtle.Turtle()
 left_pad.speed(0)
 left_pad.shape("square")
-left_pad.color("black")
+left_pad.color("blue")
 left_pad.shapesize(stretch_wid=6, stretch_len=2)
 left_pad.penup()
 left_pad.goto(-400, 0)
@@ -24,7 +63,7 @@ left_pad.goto(-400, 0)
 right_pad = turtle.Turtle()
 right_pad.speed(0)
 right_pad.shape("square")
-right_pad.color("black")
+right_pad.color("red")
 right_pad.shapesize(stretch_wid=6, stretch_len=2)
 right_pad.penup()
 right_pad.goto(400, 0)
@@ -34,7 +73,7 @@ right_pad.goto(400, 0)
 hit_ball = turtle.Turtle()
 hit_ball.speed(0)
 hit_ball.shape("circle")
-hit_ball.color("blue")
+hit_ball.color("black")
 hit_ball.penup()
 hit_ball.goto(0, 0)
 hit_ball.dx = 5
@@ -83,17 +122,37 @@ def paddlebdown():
     right_pad.sety(y)
 
 
+def paddlebleft():
+    x = right_pad.xcor()
+    x -= 20
+    right_pad.setx(x)
+
+
+def paddlebright():
+    x = right_pad.xcor()
+    x += 20
+    right_pad.setx(x)
+
+# test method
+def test():
+    print("hit right paddle")
+
 # Keyboard bindings
 sc.listen()
 sc.onkeypress(paddleaup, "e")
 sc.onkeypress(paddleadown, "x")
 sc.onkeypress(paddlebup, "Up")
 sc.onkeypress(paddlebdown, "Down")
+sc.onkeypress(paddlebleft, "Left")
+sc.onkeypress(paddlebright, "Right")
+
+# test listener
+sc.onkeypress(test, "i")
 
 
 while True:
     sc.update()
-    print(hit_ball.pos())
+    # print(hit_ball.pos())
 
     hit_ball.setx(hit_ball.xcor() + hit_ball.dx)
     hit_ball.sety(hit_ball.ycor() + hit_ball.dy)
@@ -130,17 +189,21 @@ while True:
         )
 
     # Paddle ball collision
-    if (hit_ball.xcor() > 360 and hit_ball.xcor() < 370) and (
+    rp_collision = right_pad.xcor() - 30
+    if (hit_ball.xcor() == rp_collision) and (
         hit_ball.ycor() < right_pad.ycor() + 40
         and hit_ball.ycor() > right_pad.ycor() - 40
     ):
-        hit_ball.setx(360)
-        # this can be changed here for the direction
+        
+        # test - simulate keyboard presses
+        keyboard.press('i')
+        hit_ball.setx(rp_collision)
         hit_ball.dx *= -1
 
-    if (hit_ball.xcor() < -360 and hit_ball.xcor() > -370) and (
+    lp_collision = left_pad.xcor() + 30
+    if (hit_ball.xcor() == lp_collision) and (
         hit_ball.ycor() < left_pad.ycor() + 40
         and hit_ball.ycor() > left_pad.ycor() - 40
     ):
-        hit_ball.setx(-360)
+        hit_ball.setx(lp_collision)
         hit_ball.dx *= -1
